a4d0e4953915596609efcea5c515ca9ea17be94f
Chethan Kumar <chethan.kumar@aventura.com>
2025-12-03 00:28:23 +0530

    fix: login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/auth/login",
+        "https://be-workasana-b65p.vercel.app/auth/login",
         payload
       );
       const success = response.data.message || "Signed in successfully.";
